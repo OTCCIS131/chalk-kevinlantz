a5724c65ac4ffa617d97e78b24f58fe7f1503fa3
Unknown <kl0231582@otc.edu>
2017-09-06 13:20:45 -0500

    Happy Birthday to you !!

---
+++ index.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+
+console.log(chalk.yellow('Happy birthday to you' + ' Happy birthday to you'));
+console.log(chalk.green('Happy birthday dear Adam'));
+console.log(chalk.bgGreen('Happy birthday' + ' to you'));
+console.log(chalk.bgCyan('From good friends to new'));
+console.log(chalk.bgGreen('May good luck' + ' be with you'));
+console.log(chalk.grey('Happy birthday to you!!!'));
