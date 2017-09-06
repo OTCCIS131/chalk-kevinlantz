ea4dc9477a0d9fdc2838e52110bfdf7f0161cd25
Unknown <kl0231582@otc.edu>
2017-09-06 15:21:31 -0500

    Happy Birthday to you!!! (Messing with editing )

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ console.log(chalk.yellow('Happy birthday to you' + ' Happy birthday to you'));
 console.log(chalk.green('Happy birthday dear Adam'));
 console.log(chalk.bgCyan('From good friends to new'));
 console.log(chalk.bgGreen('May good luck' + ' be with you'));
-console.log(chalk.red('Happy birthday to you!!!'));
+console.log(chalk.red('Happy birthday to you!!!!!'));
